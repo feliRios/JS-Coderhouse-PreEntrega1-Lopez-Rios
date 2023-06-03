be2f22f216f184ab4f80a609090a7815abde571d
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-06-03 20:08:30 -0300

    Armada la logica del menu y funcion calcularCuotas()

--- main.js
+++ main.js
@@ -3,44 +3,19 @@
 // - CREAR UN ALGORITMO UTILIZANDO UN CICLO
 // - ARMAR UN SIMULADOR INTERACTIVO: CARRITO DE COMPRAS
 
-function descuento(valor, porcentaje) {
-    return (valor * porcentaje) / 100
-}
-
-function cuotas(valor, cantidadCuotas) {
-    // 6 cuotas -> 15% interes
-    // 12 cuotas -> 20% interes
-    // 18 cuotas -> 22% interes
-    let valorCuota = valor / cantidadCuotas
-    if (cantidadCuotas == 6) {
-        let valorCuotaConInteres = valorCuota * 1.15;
-        let valorTotal = valorCuotaConInteres * 6;
-        return valorTotal;
-    } else if (cantidadCuotas == 12) {
-        let valorCuotaConInteres = valorCuota * 1.20;
-        let valorTotal = valorCuotaConInteres * 12;
-        return valorTotal;
-    } else if (cantidadCuotas == 18) {
-        let valorCuotaConInteres = valorCuota * 1.22
-        let valorTotal = valorCuotaConInteres * 18;
-        return valorTotal;
-    }
-    // CAMBIAR POR UN SWITCH
-}
-
-let total = 0.0, precio = 0.0, cantidad = 0;
+let total = 0.0;
+let menuDecision;  // Variable para el ciclo do-while
 
 function agregarItemCarrito(precio, cantidad) {
-    // precio = parseFloat(prompt("Ingrese el valor del producto: "));
-    // cantidad = parseInt(prompt("Ingrese la cantidad de productos: "));
     console.log("El usuario agrega " + cantidad + " productos de $" + precio);
     total += precio * cantidad;
 }
 
 function vaciarCarrito() {
-    let decision = prompt("Estas seguro que deseas vaciar el carrito? (si/no): ");
-    if (decision == "si") {
+    let vaciarDecision = prompt("Estas seguro que deseas vaciar el carrito? (si/no): ");
+    if (vaciarDecision == "si") {
         total = 0;
+        alert("Carrito vaciado con exito.");
         console.log("Vaciar carrito: el usuario vacio el carrito.");
     } else {
         console.log("Vaciar carrito: el usuario ingreso 'no' o un input no valido.");
@@ -51,3 +26,64 @@ function mostrarCarrito() {
     alert("El total del carrito es de $" + total);
 }
 
+function calcularCuotas() {
+    // 6 cuotas -> 15% interes
+    // 12 cuotas -> 20% interes
+    // 18 cuotas -> 22% interes
+    let cuotas = parseInt(prompt("Ingrese la cantidad de cuotas que desea estimar:\n 6 cuotas -> 15% interes\n 12 cuotas -> 20% interes\n 18 cuotas -> 22% interes"));
+    let valorCuota = total / cuotas;
+    let valorCuotaConInteres, valorTotal;
+
+    switch (cuotas) {
+    case 6:
+      valorCuotaConInteres = valorCuota * 1.15;
+      valorTotal = valorCuotaConInteres * 6;
+      alert("El valor del carrito en 6 cuotas es de un total de $" + valorTotal + " (6 cuotas de $" + valorCuotaConInteres + ")");
+      break;
+
+    case 12:
+      valorCuotaConInteres = valorCuota * 1.20;
+      valorTotal = valorCuotaConInteres * 12;
+      alert("El valor del carrito en 12 cuotas es de un total de $" + valorTotal + " (12 cuotas de $" + valorCuotaConInteres + ")");
+      break;
+
+    case 18:
+      valorCuotaConInteres = valorCuota * 1.22;
+      valorTotal = valorCuotaConInteres * 18;
+      alert("El valor del carrito en 18 cuotas es de un total de $" + valorTotal + " (18 cuotas de $" + valorCuotaConInteres + ")");
+      break;
+
+    default:
+      alert("Opcion invalida. Por favor, seleccione una opcion valida.");
+  }
+}
+
+do {
+    menuDecision = prompt("Elija alguna de las siguientes opciones:\n (1) Agregar productos al carrito\n (2) Mostrar el carrito\n (3) Calcular carrito en cuotas\n (4) Vaciar el carrito\n (5) Finalizar el programa");
+    switch (menuDecision) {
+        case '1':
+            let precio = parseFloat(prompt("Ingrese el valor del producto: "));
+            let cantidad = parseInt(prompt("Ingrese la cantidad de productos: "));
+            agregarItemCarrito(precio, cantidad);
+            break;
+    
+        case '2':
+            mostrarCarrito();
+            break;
+
+        case '3':
+            calcularCuotas();
+            break;
+        
+        case '4':
+            vaciarCarrito();
+            break;
+        
+        case '5':
+            break;
+        
+        default:
+            alert("Por favor, seleccione una opcion valida (evite espacios y/o numeros fuera del rango)");
+    }
+
+} while (menuDecision != 5);
